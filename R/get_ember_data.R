553b1a30d2d04f0c019e5285501ab949a623b5bf
Andy <andypicke@gmail.com>
2024-05-08 16:39:02 -0600

    specify function arguments instead of relying on order

--- R/get_ember_data.R
+++ R/get_ember_data.R
@@ -1,7 +1,7 @@
 #' Get Ember-climate data from API
 #' @param dataset Dataset to retrieve. Default = "electricity-generation". Other options are "power-sector-emissions", "electricity-demand", and "carbon-intensity"
 #' @param temporal_resolution "yearly" (default) or "monthly"
-#' @param min_date Mininum date to retrieve data for. YYYY or YYYY-MM format. Default=2015
+#' @param min_date Minimum date to retrieve data for. YYYY or YYYY-MM format. Default=2015
 #' @param max_date Maximum date to retrieve data for. YYYY or YYYY-MM format. Default=2023
 #' @param entity List of comma-separated country(s) or region(s) to return data for. Default is all (no filter)
 #' @param api_key Default is Sys.getenv("EMBER_API_KEY")
@@ -16,8 +16,12 @@ get_ember_data <- function(dataset = "electricity-generation",
                                        entity = "all",
                                        api_key = Sys.getenv("EMBER_API_KEY")) {
 
-  query_url <- construct_query_url(endpoint = dataset, temporal_resolution,
-                                   min_date, max_date, entity, api_key)
+  query_url <- construct_query_url(endpoint = dataset,
+                                   temporal_resolution = temporal_resolution,
+                                   min_date = min_date,
+                                   max_date = max_date,
+                                   entity = entity,
+                                   api_key = api_key)
 
   df <- get_api_request(query_url)
 
